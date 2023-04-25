a37868398778827c04d953874714c93bc6badf87
smdonaldson2 <smdonaldson2@bsu.edu>
2023-04-24 22:43:18 -0400

    Added css for Bus Driver page and corrected running errors

---
+++ WebMVC/Models/BusDriver.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Models
+{
+    public class BusDriver : PageModel
+    {
+        public int Boarding {get; set;}
+        public int Exiting {get; set;}
+
+        public void OnGet()
+        {
+            Boarding = 0;
+            Exiting = 0;
+        }
+
+        public void IncreaseOnBoard()
+        {
+            Boarding++;
+        }
+
+        public void DecreaseOnBoard()
+        {
+            Boarding--;
+        }
+
+        public void IncreaseDeparting()
+        {
+            Exiting++;
+        }
+
+        public void DecreaseDeparting()
+        {
+            Exiting--;
+        }
+      
+    }
+}
